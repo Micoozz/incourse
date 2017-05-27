3f677205a2fd8436a798d4ac190209ff4c587515
huang513panbin <huangpb123>
2017-05-27 16:02:20 +0800

    页面组件化

---
+++ resources/views/student/include/top_navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+  	<div id="cent_nav" class="col-md-3 col-xs-12">
+  		<ul class="col-md-12 col-xs-12">
+			<li onclick=window.open("today_homework.html","_self")>今日作业</li>
+            <li class="offt">语文</li>
+  			<li>数学</li>
+  			<li>英语</li>
+  			<li>添加课程</li>
+  		</ul>
+  	</div>
+  	<div class="col-md-6"></div>
+  	<div class="col-md-3"></div>
+</div>
