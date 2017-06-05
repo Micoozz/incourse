688e873c5cf73079aef581cbdd06adcec282741a
Micoo Zheng <939488568@qq.com>
2017-06-05 14:22:46 +0800

    add:some test-data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,23 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::table('school')->insert([
-        		'title' => '杭州第二中学',
-        		'username' => 'hangzhouerzhong',
-        		'password' => bcrypt('hz123123'),
+        		'title' => '杭州第一中学',
+        		'username' => 'hangzhouyizhong',
+        		'password' => bcrypt('123456'),
+        	]);
+        DB::table('employee')->insert([
+        		'name' => '柯元凯',
+        		'school_id' => 1,
+        		'username' => 'keyuankai',
+        		'password' => bcrypt('123456'),
+        	]);
+        DB::table('student')->insert([
+        		'name' => '郑泽果',
+        		'school_id' => 1,
+        		'class_id' => 1,
+        		'username' => 'zhengzeguo',
+        		'password' => bcrypt('123456'),
+        		'stu_number' => 1,
         	]);
     }
 }
