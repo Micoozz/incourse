18baf6292a53c682b3d915576611427c8a3cf857
Micoo Zheng <939488568@qq.com>
2017-06-08 17:36:47 +0800

    add:createExercise

--- app/Models/Objective.php
+++ app/Models/Objective.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Objective extends Model
 {
     //
+    protected $table = 'objective';
+    public $timestamps = false;
+
+    public function belongsToExercises(){
+    	return $this->belongsTo('App\Models\Exercises','exe_id');
+    }
 }
--- app/Models/Subjective.php
+++ app/Models/Subjective.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Subjective extends Model
 {
     //
+    protected $table = 'subjective';
+    public $timestamps = false;
+
+    public function belongsToExercises(){
+    	return $this->belongsTo('App\Models\Exercises','exe_id');
+    }
 }
