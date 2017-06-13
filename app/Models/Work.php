7bec2c9a7becd0356dbaaf46f0b44a7e398257d3
Micoo Zheng <939488568@qq.com>
2017-06-13 16:48:33 +0800

    add:work-type

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Work extends Model
 {
     //
+    const STATUS_OPEN = 1;
+    const STATUS_CLOSE = 2;
+    const STATUS_UNSUB = 3;
+
     protected $table = 'work';
     public $timestamps = false;
 
