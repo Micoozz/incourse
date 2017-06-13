923e50fa1641f2a8832e5fa8b9f5175bc1d1c7e1
Ren-yibo <renyibo93@gmail.com>
2017-06-13 15:18:04 +0800

    master

---
+++ AarrangementController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Http\Controllers\Teacher;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Input;
+use Auth;
+use App\Models\Job;
+class AarrangementController extends Controller
+{
+    public function showAarrangmentList(){
+       // $showAarrangmentList
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,9 @@ Route::group(['middleware' => "auth:school,employee,student"],function(){
 		Route::get('/zuoyenbenneirongliebiao',function(){
 			return view('student.zuoyenbenneirongliebiao');
 		});
+		Route::get('/cuotiben',function(){
+			reutrn view('student.cuotibenObjectiveTodayYuwen');
+		})
 	});
 	Route::get('/logout','LoginController@logout');
 });
