6d4aa5b4424eb965a5392b1b0c26da2335e2c39c
Micoo Zheng <939488568@qq.com>
2017-06-14 09:24:24 +0800

    add:showWorkDetail

--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,7 @@ Route::group(['middleware' => "auth:school,employee,student"],function(){
 			return view('student.cuotibenObjectiveTodayYuwen');
 		});
 		Route::get('/showWorkList/{page?}','Student\WorkController@showWorkList');
+		Route::get('/showWorkDetail/{work_id}/{page?}','Student\WorkController@showWorkDetail');
 	});
 	Route::get('/logout','LoginController@logout');
 });
