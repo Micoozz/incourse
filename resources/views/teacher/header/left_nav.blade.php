1440b2d464d739a724fe82e2c3b70e0d088a8700
chenjie <chenjie521vip@163.com>
2017-06-20 14:59:36 +0800

    revise

---
+++ resources/views/teacher/header/left_nav.blade.php
@@ -0,0 +1,9 @@
+                    <ul class="nav1 nav" id="nav1">
+                        <li><a href="/arrangementWork">作业管理</a></li>
+                        <li><a href="/exerciseEditor">习题库</a></li>
+                        <li><a href="">资料库</a></li>
+                        <li><a href="">班级管理</a></li>
+                        <li><a href="">成绩管理</a></li>
+                        <li><a href="">课程大纲</a></li>
+                        <li><a href="">课堂课件</a></li>
+                    </ul>
