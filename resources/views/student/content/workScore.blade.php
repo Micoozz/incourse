29e31c2ad1e6726929344bedb3ad37946bffe71b
Ren-yibo <renyibo93@gmail.com>
2017-10-20 13:42:01 +0800

    student

--- resources/views/student/content/workScore.blade.php
+++ resources/views/student/content/workScore.blade.php
@@ -49,12 +49,12 @@
 			@if(empty($data['sameExercise']))
 				<button class="btn-white" onclick="window.location.href = '/learningCenter/{{ $courseFirst[0]['id'] }}'" >返回</button>
 				@if(!empty($tutorship))
-					<button class="ic-btn" id="work-parsing" onclick="window.location.href = '/learningCenter/{{ $courseFirst[0]['id'] }}/{{ $mod }}/error_reports/{{ $parameter }}'" >错题解析</button>
+					<button class="ic-btn" id="work-parsing">错题解析</button>
 					<button class="ic-btn error-exercise" error-exercise="{{ $tutorship }}">分数提升</button>
 				@endif	
 			@else
 				<button class="ic-btn" onclick="window.location.href = '/learningCenter/{{ $courseFirst[0]['id'] }}/{{ $mod }}/exercise_book/{{ $parameter }}'" >返回</button>
-				<button class="ic-btn" id="work-parsing" onclick="window.location.href='/learningCenter/{{ $courseFirst[0]['id'] }}/{{ $mod }}/error_reports/{{ $parameter }}'">错题解析</button>
+				<button class="ic-btn" id="work-parsing">错题解析</button>
 			@endif
 		</div>
 	</div>
