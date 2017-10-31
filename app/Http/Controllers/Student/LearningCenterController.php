96cf55a1b4717cd0d8c84b2c45317be2e8630c1b
Ren-yibo <renyibo93@gmail.com>
2017-10-31 19:26:29 +0800

    student

--- app/Http/Controllers/Student/LearningCenterController.php
+++ app/Http/Controllers/Student/LearningCenterController.php
@@ -250,6 +250,7 @@ class LearningCenterController extends Controller
 					if (empty($same_list->toArray())) {
 						$data['exeSecond'] = $this->changeTimeType($second);
 						$tutorship = isset($tutorship) ? implode('&',$tutorship) : null;//所有的错题ID
+						dump($correctScore);dd($totalScore);
 						$accuracy = $correctScore / $totalScore;//这里算分数率，
 					}else{
 						$sameSecond = 0;
