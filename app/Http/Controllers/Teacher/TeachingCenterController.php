e24393a57997f16a089f8010ef8ade7a2b17c45e
Micoo Zheng <939488568@qq.com>
2017-11-03 18:59:37 +0800

    hotfix

--- app/Http/Controllers/Teacher/TeachingCenterController.php
+++ app/Http/Controllers/Teacher/TeachingCenterController.php
@@ -110,7 +110,8 @@ class TeachingCenterController extends TeacherController
         foreach($job_list as $job){
             $job->sub_count = $job->hasManyWork()->where('status','>=',2)->get()->count();
             $job->count = $job->hasManyWork()->get()->count();
-
+            $avg_score = $job->hasManyWork()->where('status','>=',2)->sum('score') / $job->sub_count;
+            $job->avg_ratings = round($avg_score / $job->score,2);
         }
         // $job_section_list = Job::where(['teacher_id' => $teacher->id,'job_type' => $type,'class_id' => $class_id])->pluck('chapter_id');
         // $section_id_list = Chapter::whereIn('id',$job_section_list)->pluck('parent_id');
@@ -127,6 +128,7 @@ class TeachingCenterController extends TeacherController
             $student = Student::find($work->student_id);
             $work->student_name = $student->name;
             $work->total =$work->belongsToJob()->first()->score;
+            $work->ratings = round($work->score / $work->total,2);
         }
         return view('teacher.content.correct_work',compact("title",'class_course','class_id','course_id','work_list'));
     }
