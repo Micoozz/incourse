78207dc49ced99cff91a4405f92b28a47f1117b6
Micoo Zheng <939488568@qq.com>
2017-11-04 18:24:50 +0800

    merge

---
+++ app/Models/CoursewareTeacherCourseMap.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CoursewareTeacherCourseMap extends Model
+{
+    //
+    protected $table = 'courseware_teacher_course_map';
+    public $timestamps = false;
+}
