fc306b55d452f9114e08f0839585083515a5a4c4
ZhangYao <724400475@qq.com>
2017-11-07 11:08:32 +0800

    merge

--- resources/views/teacher/template/courseware_left.blade.php
+++ resources/views/teacher/template/courseware_left.blade.php
@@ -1,8 +1,8 @@
 <ul class="nav1 nav" id="nav1">
-    <li><a href="/homeworkManage/{{$class_id}}/{{$course_id}}" num='1'>作业管理</a></li>
-    <li><a href="javascript:;" num='3'>资料库</a></li>
+    <li><a href="/homeworkManage/{{$class_id}}/{{$course_id}}" style="color:#333" num='1'>作业管理</a></li>
+    <!-- <li><a href="javascript:;" num='3'>资料库</a></li>
     <li><a href="javascript:;" num='4'>班级管理</a></li>
     <li><a href="javascript:;" num='5'>成绩管理</a></li>
-    <li><a href="javascript:;" num='6'>课程大纲</a></li>
+    <li><a href="javascript:;" num='6'>课程大纲</a></li> -->
     <li><a href="/courseWare/main/{{$class_id}}/{{$course_id}}" class="box" num='7'>课堂课件</a></li>
 </ul>
